0ef9e7036565ce6221692bc85858e45af8d37957
amigin <amigin@gmail.com>
2022-12-20 18:33:54 +0200

    TableName is get from Entity

--- src/my_no_sql_entity.rs
+++ src/my_no_sql_entity.rs
@@ -1,5 +1,11 @@
+pub struct TableInfo {
+    pub table_name: &'static str,
+}
+
 pub trait MyNoSqlEntity {
     fn get_partition_key(&self) -> &str;
     fn get_row_key(&self) -> &str;
     fn get_time_stamp(&self) -> i64;
+
+    fn get_table_info() -> TableInfo;
 }
